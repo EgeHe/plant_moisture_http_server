53cf187b718956ada9c523f3734525908987f9dc
Eero Heinänen <eero.heinanen@hotmail.com>
2019-03-17 14:13:26 +0200

    Add empty project files

---
+++ main.py
@@ -0,0 +1,4 @@
+import plant_moisture_http_server
+
+if __name__ == '__main__':
+    pass
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plant_moisture_http_server',
+    version='0.0.1',
+    packages=find_packages(),
+    url='',
+    license='MIT',
+    author='Eero Heinänen',
+    author_email='eero.heinanen@hotmail.com',
+    description=''
+)
